f658124b79f564b803e270de999c143f20e77e85
Wes Hardaker <hardaker@users.sourceforge.net>
1995-03-14 18:06:29 +0000

    hpux.c and var_struct.h got its content twice somehow (cvs merge).
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/HP@121 06827809-a52a-0410-b366-d66718629ded

--- snmplib/hpux.c
+++ snmplib/hpux.c
@@ -13,18 +13,3 @@ void srandom(seed)
 }
 
 #endif
-/* hp-port:  needed random defs and htonl and htons defs */
-
-#ifdef hpux
-int random()
-{
-  return(rand());
-}
-
-void srandom(seed)
-  unsigned int seed;
-{
-  srand(seed);
-}
-
-#endif
