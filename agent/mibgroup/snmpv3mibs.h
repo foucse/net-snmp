111323b24dba5f688ee338fb9eab164a47adbd56
Wes Hardaker <hardaker@users.sourceforge.net>
1998-09-15 00:49:15 +0000

    - (snmpv3mibs.h): high-level module created to import v3 releated mibs.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V3-b1@1694 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/mibgroup/snmpv3mibs.h
@@ -0,0 +1,9 @@
+#ifndef SNMPV3MIBS_H
+#define SNMPV3MIBS_H
+
+/* snmpv3mibs.h: mib module to include the modules relavent to the
+   snmpv3 mib(s) */
+
+config_require(snmpv3/snmpEngine)
+
+#endif /* NSMPV3MIBS_H */
