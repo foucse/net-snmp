80e653a78d60a5b6a8fc68971b736fdd9dd868de
Wes Hardaker <hardaker@users.sourceforge.net>
1998-10-26 05:00:15 +0000

    - (system.h): created DEBUGPL to do DEBUGP with file/line statements.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V3-b1@1918 06827809-a52a-0410-b366-d66718629ded

--- snmplib/system.h
+++ snmplib/system.h
@@ -72,6 +72,8 @@ void DEBUGP __P((const char *, ...));
 void DEBUGP __P((va_alist));
 #endif
 
+#define DEBUGPL(x) DEBUGP("%s:%d:",__FILE__,__LINE__); DEBUGP x 
+
 #ifndef HAVE_STRDUP
 char *strdup __P((char *));
 #endif
