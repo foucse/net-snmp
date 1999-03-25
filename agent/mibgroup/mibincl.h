2046b47158ff40258860f262db461668b75104df
Wes Hardaker <hardaker@users.sourceforge.net>
1999-03-25 04:30:12 +0000

    - (mibincl.h): add agent_read_config.h
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V3-b3@2459 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/mibincl.h
+++ agent/mibgroup/mibincl.h
@@ -13,6 +13,7 @@
 #include "../../snmplib/snmp_api.h"
 #include "../../snmplib/snmp_impl.h"
 #include "../snmp_vars.h"
+#include "../agent_read_config.h"
 #include "../var_struct.h"
 #include "../../snmplib/snmp.h"
 #include "../../snmplib/mib.h"
