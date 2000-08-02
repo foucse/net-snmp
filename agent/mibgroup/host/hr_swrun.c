3b075593c82dd9eed2c7800da4fc24f4163a27c7
Niels Baggesen <nba@users.sourceforge.net>
2000-08-02 15:28:25 +0000

    - hr_swrun.c: fix possible buffer overrun
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/V4-1-1-patches@4391 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/host/hr_swrun.c
+++ agent/mibgroup/host/hr_swrun.c
@@ -456,7 +456,7 @@ var_hrswrun(struct variable *vp,
 	    sprintf( string, "/proc/%d/cmdline", pid );
 	    if ((fp = fopen( string, "r")) == NULL) return NULL;
 	    memset( buf, 0, sizeof(buf) );
-	    fgets( buf, sizeof(buf), fp );   /* argv[0] '\0' argv[1] '\0' .... */
+	    fgets( buf, sizeof(buf)-1, fp );   /* argv[0] '\0' argv[1] '\0' .... */
 
 		/* Skip over argv[0] */
 	    cp = buf;
