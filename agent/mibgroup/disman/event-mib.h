38b015b24e6fdbdad64f010c19c686ba7c784300
Wes Hardaker <hardaker@users.sourceforge.net>
2001-06-14 14:45:10 +0000

    This commit was manufactured by cvs2svn to create branch 'NEW-LIBRARY-API'.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/NEW-LIBRARY-API@5338 06827809-a52a-0410-b366-d66718629ded

--- agent/mibgroup/disman/event-mib.h
+++
@@ -1,8 +0,0 @@
-/* wrapper for the disman event mib code files */
-config_require(disman/mteTriggerTable)
-config_require(disman/mteTriggerDeltaTable)
-config_require(disman/mteTriggerExistenceTable)
-config_require(disman/mteTriggerBooleanTable)
-config_require(disman/mteTriggerThresholdTable)
-config_require(disman/mteObjectsTable)
-config_add_mib(DISMAN-EVENT-MIB)
--- s/openbsd.h
+++
@@ -1,6 +0,0 @@
-#include "netbsd.h"
-
-#define netbsd1 1 /* we're really close to this */
-
-#undef MBPOOL_SYMBOL
-#undef MCLPOOL_SYMBOL
