512485b5369603c4c362eff329c33a7e79f7c1b8
Wes Hardaker <hardaker@users.sourceforge.net>
2001-11-13 07:19:59 +0000

    Add simply "instrument this int/u_long/counter" type functions. Likely to change.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/NEW-AGENT-API@5812 06827809-a52a-0410-b366-d66718629ded

--- agent/helpers/instance.c
+++ agent/helpers/instance.c
@@ -36,6 +36,227 @@ register_read_only_instance(handler_registration *reginfo) {
 }
 
 int
+register_read_only_ulong_instance(const char *name,
+                                  oid *reg_oid, size_t reg_oid_len,
+                                  u_long *it) 
+{
+    handler_registration *myreg =
+        create_handler_registration(name,
+                                    instance_ulong_handler,
+                                    reg_oid, reg_oid_len,
+                                    HANDLER_CAN_RONLY);
+    myreg->handler->myvoid = (void *) it;
+    return register_read_only_instance(myreg);
+}
+
+int
+register_ulong_instance(const char *name,
+                        oid *reg_oid, size_t reg_oid_len,
+                        u_long *it) 
+{
+    handler_registration *myreg =
+        create_handler_registration(name,
+                                    instance_ulong_handler,
+                                    reg_oid, reg_oid_len,
+                                    HANDLER_CAN_RWRITE);
+    myreg->handler->myvoid = (void *) it;
+    return register_instance(myreg);
+}
+
+int
+register_read_only_counter32_instance(const char *name,
+                                      oid *reg_oid, size_t reg_oid_len,
+                                      u_long *it) 
+{
+    handler_registration *myreg =
+        create_handler_registration(name,
+                                    instance_counter32_handler,
+                                    reg_oid, reg_oid_len,
+                                    HANDLER_CAN_RONLY);
+    myreg->handler->myvoid = (void *) it;
+    return register_read_only_instance(myreg);
+}
+
+int
+register_read_only_long_instance(const char *name,
+                                 oid *reg_oid, size_t reg_oid_len,
+                                 long *it) 
+{
+    handler_registration *myreg =
+        create_handler_registration(name,
+                                    instance_long_handler,
+                                    reg_oid, reg_oid_len,
+                                    HANDLER_CAN_RONLY);
+    myreg->handler->myvoid = (void *) it;
+    return register_read_only_instance(myreg);
+}
+
+int
+register_long_instance(const char *name,
+                       oid *reg_oid, size_t reg_oid_len,
+                       long *it) 
+{
+    handler_registration *myreg =
+        create_handler_registration(name,
+                                    instance_long_handler,
+                                    reg_oid, reg_oid_len,
+                                    HANDLER_CAN_RWRITE);
+    myreg->handler->myvoid = (void *) it;
+    return register_instance(myreg);
+}
+
+int
+instance_ulong_handler(
+    mib_handler               *handler,
+    handler_registration      *reginfo,
+    agent_request_info        *reqinfo,
+    request_info              *requests) {
+
+    u_long *it = (u_long *) handler->myvoid;
+    u_long *it_save;
+    
+    DEBUGMSGTL(("instance_ulong_handler", "Got request:  %d\n", reqinfo->mode));
+
+    switch(reqinfo->mode) {
+        /* data requests */
+        case MODE_GET:
+            snmp_set_var_typed_value(requests->requestvb, ASN_UNSIGNED,
+                                     (u_char *) it,
+                                     sizeof(*it));
+            break;
+
+        /* SET requests.  Should only get here if registered RWRITE */
+        case MODE_SET_RESERVE1:
+            if (requests->requestvb->type != ASN_UNSIGNED)
+                set_request_error(reqinfo, requests, SNMP_ERR_WRONGTYPE);
+            break;
+
+        case MODE_SET_RESERVE2:
+            /* store old info for undo later */
+            memdup((u_char **) &it_save,
+                   (u_char *) it, sizeof(u_long));
+            if (it_save == NULL) {
+                set_request_error(reqinfo, requests,
+                                  SNMP_ERR_RESOURCEUNAVAILABLE);
+                return SNMP_ERR_NOERROR;
+            }
+            request_add_list_data(requests,
+                                  create_data_list(INSTANCE_HANDLER_NAME,
+                                                   it_save, free));
+            break;
+
+        case MODE_SET_ACTION:
+            /* update current */
+            DEBUGMSGTL(("testhandler","updated u_long %ul -> %ul\n", *it,
+                        *(requests->requestvb->val.integer)));
+            *it = *(requests->requestvb->val.integer);
+            break;
+            
+        case MODE_SET_UNDO:
+            *it =
+                *((u_long *) request_get_list_data(requests,
+                                                   INSTANCE_HANDLER_NAME));
+            break;
+
+        case MODE_SET_COMMIT:
+        case MODE_SET_FREE:
+                /* nothing to do */
+            break;
+    }
+    return SNMP_ERR_NOERROR;
+}
+
+int
+instance_counter32_handler(
+    mib_handler               *handler,
+    handler_registration      *reginfo,
+    agent_request_info        *reqinfo,
+    request_info              *requests) {
+
+    u_long *it = (u_long *) handler->myvoid;
+    u_long *it_save;
+    
+    DEBUGMSGTL(("instance_ulong_handler", "Got request:  %d\n", reqinfo->mode));
+
+    switch(reqinfo->mode) {
+        /* data requests */
+        case MODE_GET:
+            snmp_set_var_typed_value(requests->requestvb, ASN_COUNTER,
+                                     (u_char *) it,
+                                     sizeof(*it));
+            break;
+
+        /* SET requests.  Should only get here if registered RWRITE */
+        default:
+            snmp_log(LOG_ERR,"instance_counter32_handler: illegal mode\n");
+            set_request_error(reqinfo, requests, SNMP_ERR_GENERR);
+            return SNMP_ERR_NOERROR;
+    }
+    return SNMP_ERR_NOERROR;
+}
+
+int
+instance_long_handler(
+    mib_handler               *handler,
+    handler_registration      *reginfo,
+    agent_request_info        *reqinfo,
+    request_info              *requests) {
+
+    long *it = (u_long *) handler->myvoid;
+    long *it_save;
+    
+    DEBUGMSGTL(("instance_ulong_handler", "Got request:  %d\n", reqinfo->mode));
+
+    switch(reqinfo->mode) {
+        /* data requests */
+        case MODE_GET:
+            snmp_set_var_typed_value(requests->requestvb, ASN_INTEGER,
+                                     (u_char *) it,
+                                     sizeof(*it));
+            break;
+
+        /* SET requests.  Should only get here if registered RWRITE */
+        case MODE_SET_RESERVE1:
+            if (requests->requestvb->type != ASN_UNSIGNED)
+                set_request_error(reqinfo, requests, SNMP_ERR_WRONGTYPE);
+            break;
+
+        case MODE_SET_RESERVE2:
+            /* store old info for undo later */
+            memdup((u_char **) &it_save,
+                   (u_char *) it, sizeof(u_long));
+            if (it_save == NULL) {
+                set_request_error(reqinfo, requests,
+                                  SNMP_ERR_RESOURCEUNAVAILABLE);
+                return SNMP_ERR_NOERROR;
+            }
+            request_add_list_data(requests,
+                                  create_data_list(INSTANCE_HANDLER_NAME,
+                                                   it_save, free));
+            break;
+
+        case MODE_SET_ACTION:
+            /* update current */
+            DEBUGMSGTL(("testhandler","updated u_long %ul -> %ul\n", *it,
+                        *(requests->requestvb->val.integer)));
+            *it = *(requests->requestvb->val.integer);
+            break;
+            
+        case MODE_SET_UNDO:
+            *it =
+                *((u_long *) request_get_list_data(requests,
+                                                   INSTANCE_HANDLER_NAME));
+            break;
+
+        case MODE_SET_COMMIT:
+        case MODE_SET_FREE:
+                /* nothing to do */
+            break;
+    }
+    return SNMP_ERR_NOERROR;
+}
+
+int
 instance_helper_handler(
     mib_handler               *handler,
     handler_registration      *reginfo,
--- agent/helpers/instance.h
+++ agent/helpers/instance.h
@@ -13,7 +13,26 @@
 int register_instance(handler_registration *reginfo);
 int register_read_only_instance(handler_registration *reginfo);
 
+#define INSTANCE_HANDLER_NAME "instance"
+
 mib_handler *get_instance_handler(void);
    
-NodeHandler instance_helper_handler;
+int register_read_only_ulong_instance(const char *name,
+                                      oid *reg_oid, size_t reg_oid_len,
+                                      u_long *it);
+int register_ulong_instance(const char *name, oid *reg_oid, size_t reg_oid_len,
+                            u_long *it);
+int register_read_only_counter32_instance(const char *name,
+                                          oid *reg_oid, size_t reg_oid_len,
+                                          u_long *it);
+int register_read_only_long_instance(const char *name,
+                                     oid *reg_oid, size_t reg_oid_len,
+                                     long *it);
+int register_long_instance(const char *name, oid *reg_oid, size_t reg_oid_len,
+                           long *it);
 
+
+NodeHandler instance_helper_handler;
+NodeHandler instance_ulong_handler;
+NodeHandler instance_long_handler;
+NodeHandler instance_counter32_handler;
