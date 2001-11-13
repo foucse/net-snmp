84fb4fc3ab642a52c986e3775eeebeff6982f774
Wes Hardaker <hardaker@users.sourceforge.net>
2001-11-13 07:19:32 +0000

    wrappers for all handlers.
    git-svn-id: file:///home/hardaker/lib/sf-bkups/net-snmp-convert-svnrepo/branches/NEW-AGENT-API@5811 06827809-a52a-0410-b366-d66718629ded

---
+++ agent/helpers/all_helpers.c
@@ -0,0 +1,11 @@
+/** @addgroup handler @{ */
+
+
+/** call the initialization sequence for all handlers with init_ routines. */
+void
+init_helpers(void) 
+{
+    init_serialize();
+    init_read_only_helper();
+}
+
